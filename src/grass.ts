613ba319128c80f121f10857eb74ef9c239f4b44
Alexander Metsker <alex.petsker@gmail.com>
2023-08-12 11:58:53 +0200

    Grass and models added

---
+++ src/grass.ts
@@ -0,0 +1,109 @@
+import * as THREE from 'three';
+
+
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler';
+class Grass extends THREE.Group {
+
+    instances: number;
+    plane : THREE.Object3D 
+    triangle : THREE.Mesh;
+    grassInstance : THREE.InstancedMesh;
+    sampler: MeshSurfaceSampler | undefined;
+
+    _positionx: number[];
+    _positiony: number[];
+    _positionz: number[];
+
+    GrassMaterial: THREE.MeshPhongMaterial;
+
+    dummy: THREE.Object3D;
+
+    constructor(instances : number, plane : THREE.Mesh, triangle : THREE.Mesh) {
+        super();
+        this.instances = instances;
+        this.plane = plane;
+        this.dummy = new THREE.Object3D;
+        this.triangle = triangle;
+        this.triangle.geometry.rotateX(-Math.PI /2)
+        this.GrassMaterial = new THREE.MeshPhongMaterial({
+            side: THREE.DoubleSide,
+            vertexColors: true,
+            specular: 0,
+            reflectivity: 0,
+            shininess:0
+        });
+
+        triangle.geometry.computeVertexNormals();
+        triangle.geometry.attributes.normal.needsUpdate = true;
+        triangle.geometry.getAttribute('normal');
+        this.setNormal(triangle.geometry, 0);
+        this.setNormal(triangle.geometry, 1);
+        this.setNormal(triangle.geometry, 2);
+
+
+        this.sampler = new MeshSurfaceSampler( plane )
+        .setWeightAttribute( 'uv' )
+        .build();   
+        this.grassInstance = new THREE.InstancedMesh( triangle.geometry, this.GrassMaterial, this.instances );
+        
+        this._positionx = [instances];
+        this._positiony = [instances];
+        this._positionz = [instances];
+
+        let _tempPosition = new THREE.Vector3;
+        for ( let i = 0; i < instances; i ++ ) {
+
+            this.sampler.sample( _tempPosition );
+            this.dummy.position.copy( new THREE.Vector3(_tempPosition.x *100,0 *100,_tempPosition.y *100));
+            this.dummy.scale.set(100,100,100);
+            this.dummy.updateMatrix();
+
+            this.grassInstance.setMatrixAt( i, this.dummy.matrix );
+
+            this._positionx[i] = _tempPosition.x;
+            this._positiony[i] = _tempPosition.y;
+            this._positionz[i] = _tempPosition.z;
+        }
+
+        this.grassInstance.receiveShadow = true;
+        this.grassInstance.instanceMatrix.needsUpdate = true;
+        this.add(this.grassInstance);
+        this.add(this.plane);
+
+    }
+    
+
+    resampleParticle(camera: THREE.Camera ) {
+        const dummy = new THREE.Object3D();
+        
+        dummy.updateMatrix()
+        for ( let i = 0; i < this.instances; i ++ ) {
+            dummy.position.copy( new THREE.Vector3(this._positionx[i] *100,this._positionz[i]*100,this._positiony[i] *100));
+            dummy.scale.set(100,100,100);
+            dummy.lookAt(new THREE.Vector3(camera.position.x,0,camera.position.z))
+            dummy.updateMatrix();
+    
+            
+            dummy.updateMatrix();
+            this.grassInstance.setMatrixAt( i, dummy.matrix );
+            this.grassInstance.instanceMatrix.needsUpdate = true;
+        }
+    }
+
+    setNormal = function (geometry, normalIndex) {
+        const pos = {
+            x: 0,
+            y: 1,
+            z: 0
+        };
+        const normal = geometry.getAttribute('normal');
+        normal.array[normalIndex * 3] = pos.x;
+        normal.array[normalIndex * 3 + 1] = pos.y;
+        normal.array[normalIndex * 3 + 2] = pos.z;
+        normal.needsUpdate = true;
+    };
+
+
+}
+
+export { Grass };
--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,30 @@ import { MapControls, OrbitControls } from "three/examples/jsm/controls/OrbitCon
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 
+import grassUrl from "./assets/models/grass.fbx"
+import triangleUrl from "./assets/models/triangle.fbx"
+import blenderUrl from "./assets/models/blender.fbx"
+import pillerUrl from "./assets/models/piller.fbx"
 
-import HelloWorldPass from "./HelloWorldPass"
+import { Grass } from "./grass"
+
 import RenderPixelatedPass from "./RenderPixelatedPass"
 import PixelatePass from "./PixelatePass"
 
 import { stopGoEased } from "./math"
 
-import warningStipesURL from "./assets/warningStripes.png"
-import crateURL from "./assets/TileCrate.png"
-import mechURL from "./assets/mech.fbx"
 
+
+let pointlight = new THREE.PointLight(0x0000FF,2);
 let camera: THREE.Camera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, composer: EffectComposer
 let controls: OrbitControls
-let crystalMesh: THREE.Mesh, mech: THREE.Object3D
+let crystalMesh: THREE.Mesh, plane: THREE.Object3D, mesh: THREE.Mesh
+let grassPlane: Grass
 
 init()
-animate()
-
+setTimeout( function() { animate(); }, 100 );
 function init() {
 
     let screenResolution = new Vector2( window.innerWidth, window.innerHeight )
@@ -42,8 +44,8 @@ function init() {
 
     // Renderer
     renderer = new THREE.WebGLRenderer( { antialias: false } )
-    // renderer.toneMapping = THREE.ACESFilmicToneMapping
-    // renderer.toneMappingExposure = .75
+    renderer.toneMapping = THREE.ACESFilmicToneMapping
+    renderer.toneMappingExposure = .75
     renderer.shadowMap.enabled = true
     renderer.setSize( screenResolution.x, screenResolution.y )
     document.body.appendChild( renderer.domElement )
@@ -57,137 +59,118 @@ function init() {
 
     controls = new OrbitControls( camera, renderer.domElement )
     controls.target.set( 0, 0, 0 )
-    camera.position.z = 2
-    camera.position.y = 2 * Math.tan( Math.PI / 6 )
+    camera.position.z = 4
+    camera.position.y = 6 * Math.tan( Math.PI / 6 )
     controls.update()
     // controls.minPolarAngle = controls.maxPolarAngle = controls.getPolarAngle()
 
-    const texLoader = new THREE.TextureLoader()
-    const tex_crate = pixelTex( texLoader.load( crateURL ) )
-    const tex_warningStripes = pixelTex( texLoader.load( warningStipesURL ) )
-    const tex_checker = pixelTex( texLoader.load( "https://threejsfundamentals.org/threejs/resources/images/checker.png" ) )
-    const tex_checker2 = pixelTex( texLoader.load( "https://threejsfundamentals.org/threejs/resources/images/checker.png" ) )
-    tex_checker.repeat.set( 3, 3 )
-    tex_checker2.repeat.set( 1.5, 1.5 )
 
     // Geometry
-    // {
-    //     const fbxLoader = new FBXLoader()
-    //     let mechMaterial = new THREE.MeshPhongMaterial( {
-    //         // map: tex_checker2,
-    //         specular: 0xffffff,
-    //         shininess: 100
-    //     } )
-    //     fbxLoader.load( mechURL, obj => {
-    //         // obj.scale.set( .005, .005, .005 )
-    //         obj.scale.set( .001, .001, .001 )
-    //         obj.traverse( child => {
-    //             // @ts-ignore
-    //             if ( child instanceof THREE.Mesh ) {
-    //                 child.castShadow = true
-    //                 child.receiveShadow = true
-    //                 child.material = mechMaterial
-    //             }
-    //         } )
-    //         console.log( obj )
-    //         mech = obj
-    //         scene.add( obj )
-    //     } )
-    // }
+
+    let GrassMaterial = new THREE.MeshPhongMaterial({
+        side: THREE.FrontSide,
+        color: 0x228822,
+        specular:0,
+        reflectivity : 0,
+        shininess :0
+                
+        
+         // Render both sides of the plane
+    });
+
     {
-        // let boxMaterial = new THREE.MeshNormalMaterial()
-        let boxMaterial = new THREE.MeshPhongMaterial( { map: tex_checker2 } )
-        // let boxMaterial = new THREE.MeshPhongMaterial()
-        function addBox( boxSideLength: number, x: number, z: number, rotation: number ) {
-            let mesh = new THREE.Mesh( new THREE.BoxGeometry( boxSideLength, boxSideLength, boxSideLength ), boxMaterial )
-            mesh.castShadow = true
-            mesh.receiveShadow = true
-            mesh.rotation.y = rotation
-            mesh.position.y = boxSideLength / 2
-            mesh.position.set( x, boxSideLength / 2 + .0001, z )
-            scene.add( mesh )
-            return mesh
-        }
-        addBox( .4, 0, 0, Math.PI / 4 )
-        addBox( .2, -.4, -.15, Math.PI / 4 )
-    }
-    {
-        const planeSideLength = 2
-        let planeMesh = new THREE.Mesh(
-            new THREE.PlaneGeometry( planeSideLength, planeSideLength ),
-            new THREE.MeshPhongMaterial( {
-                map: tex_checker,
-                // side: THREE.DoubleSide
+        const fbxLoader = new FBXLoader()
+        fbxLoader.load( grassUrl, obj => {
+            obj.traverse( child => {
+                // @ts-ignore
+                if ( child instanceof THREE.Mesh ) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                    child.material = GrassMaterial;
+                    mesh = child;
+                }
             } )
-            // new THREE.MeshPhongMaterial( { side: THREE.DoubleSide } )
-        )
-        planeMesh.receiveShadow = true
-        planeMesh.rotation.x = -Math.PI / 2
-        scene.add( planeMesh )
-    }
-    {
-        const radius = .2
-        // const geometry = new THREE.DodecahedronGeometry( radius )
-        const geometry = new THREE.IcosahedronGeometry( radius )
-        crystalMesh = new THREE.Mesh(
-            geometry,
-            new THREE.MeshPhongMaterial( {
-                color: 0x2379cf,
-                emissive: 0x143542,
-                shininess: 100,
-                specular: 0xffffff,
-                // opacity: 0.5
-            } )
-            // new THREE.MeshNormalMaterial()
-        )
-        crystalMesh.receiveShadow = true
-        crystalMesh.castShadow = true
-        scene.add( crystalMesh )
+        } )
     }
 
-    // Lights
-    scene.add( new THREE.AmbientLight( 0x2d3645, 1.5 ) )
     {
-        let directionalLight = new THREE.DirectionalLight( 0xfffc9c, .5 )
+        const fbxLoader = new FBXLoader()
+        fbxLoader.load( blenderUrl, obj => {
+            obj.traverse( child => {
+                // @ts-ignore
+                if ( child instanceof THREE.Mesh ) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                    crystalMesh = child;
+
+                }
+            } )
+            scene.add(obj);
+        } )
+    }
+
+    {
+        const fbxLoader = new FBXLoader()
+        fbxLoader.load( pillerUrl, obj => {
+            obj.traverse( child => {
+                // @ts-ignore
+                if ( child instanceof THREE.Mesh ) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                }
+            } )
+            obj.position.y =+ 0.1;
+            scene.add(obj);
+        } )
+    }
+
+    {
+        
+        const fbxLoader = new FBXLoader()
+        fbxLoader.load( triangleUrl, obj => {
+            obj.traverse( child => {
+                // @ts-ignore
+                if ( child instanceof THREE.Mesh ) {
+
+                    child.castShadow = true
+                    child.receiveShadow = true
+                    child.updateMatrix()
+                    grassPlane = new Grass(15000,mesh,child);
+
+                    scene.add(grassPlane);
+                }
+            } )
+        } )
+    }
+
+
+    // Lights
+    scene.add( new THREE.AmbientLight( 0x2d3645, 0.5 ) )
+    {
+        let directionalLight = new THREE.DirectionalLight( 0x506886, .1 )
         directionalLight.position.set( 100, 100, 100 )
         directionalLight.castShadow = true
-        // directionalLight.shadow.radius = 0
+        directionalLight.shadow.radius = 0
         directionalLight.shadow.mapSize.set( 2048, 2048 )
         scene.add( directionalLight )
     }
-    {
-        let spotLight = new THREE.SpotLight( 0xff8800, 1, 10, Math.PI / 16, .02, 2 )
-        // let spotLight = new THREE.SpotLight( 0xff8800, 1, 10, Math.PI / 16, 0, 2 )
-        spotLight.position.set( 2, 2, 0 )
-        let target = spotLight.target //= new THREE.Object3D()
-        scene.add( target )
-        target.position.set( 0, 0, 0 )
-        spotLight.castShadow = true
-        scene.add( spotLight )
-        // spotLight.shadow.radius = 0
-    }
 }
+
+
 
 function animate() {
     requestAnimationFrame( animate )
     let t = performance.now() / 1000
 
+    
+
+    grassPlane.resampleParticle(camera)
+
     let mat = ( crystalMesh.material as THREE.MeshPhongMaterial )
     mat.emissiveIntensity = Math.sin( t * 3 ) * .5 + .5
     crystalMesh.position.y = .7 + Math.sin( t * 2 ) * .05
-    // crystalMesh.rotation.y = stopGoEased( t, 3, 4 ) * Math.PI / 2
     crystalMesh.rotation.y = stopGoEased( t, 2, 4 ) * 2 * Math.PI
-
-    // if ( mech )
-    //     mech.rotation.y = Math.floor( t * 8 ) * Math.PI / 32
+    pointlight.position.copy(crystalMesh.position);
+    scene.add(pointlight)
     composer.render()
-}
-
-function pixelTex( tex: THREE.Texture ) {
-    tex.minFilter = THREE.NearestFilter
-    tex.magFilter = THREE.NearestFilter
-    tex.generateMipmaps = false
-    tex.wrapS = THREE.RepeatWrapping
-    tex.wrapT = THREE.RepeatWrapping
-    return tex
 }
